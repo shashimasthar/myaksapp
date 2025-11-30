f77526d31eee9177f5a4b7bf8a092be341deec4f
shashimasthar <40067644+shashimasthar@users.noreply.github.com>
2025-11-30 10:50:55 +0530

    VM
    VM

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,6 +27,19 @@ resource "azurerm_container_registry" "acr" {
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = var.aks_name
   location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "aksapp"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2s_v4"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
   tags = {
     Environment = "Demo"
   }
