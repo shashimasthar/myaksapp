48f575db672e75572e00ad65142b82cbeb43f8c7
shashimasthar <40067644+shashimasthar@users.noreply.github.com>
2025-11-30 10:26:06 +0530

    vm chnage
    vm chnage

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,7 +33,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name       = "default"
     node_count = 1
-    vm_size    = "Standard_DS2_v2" # More widely available than B-series
+    vm_size    = "Standard_DS2_v4" # More widely available than B-series
   }
 
   identity {
